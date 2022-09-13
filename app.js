3358efafdd205ef428830e586ba9b0169434c8d9
Denis Pablo <denispablo17@gmail.com>
2022-09-12 23:14:26 -0300

    Eliminacion de cartas

--- app.js
+++ app.js
@@ -1,32 +1,33 @@
-let url = "http://hp-api.herokuapp.com/api/characters";
+function comenzar() {
+  let url = "http://hp-api.herokuapp.com/api/characters";
 
-fetch(url)
-  .then((response) => response.json())
-  .then((data) => mostrarData(data))
-  .catch((error) => console.log(error));
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) =>
+      mostrarData(data.filter((personaje) => personaje.image != ""))
+    )
+    .catch((error) => console.log(error));
+
+  document.getElementById("inicio").style.display = "none";
+}
+
+comenzar();
 
 function mostrarData(data) {
   let cartas = [];
-  let cantidadPersonajes = 0;
   let id = 0;
 
-  while (cantidadPersonajes < 12) {
-    let n = Math.round(Math.random() * 403);
-
-    if (data[n].image != "") {
-      id++;
-      let carta = `<td><button id="${id}" class="carta" onclick="voltear(${id})"><img src="${data[n].image}"></img></button></td>`;
-
-      let cartaDuplicada = `<td><button id="${id}" class="carta" onclick="voltear(${id})"><img src="${data[n].image}"></img></button></td>`;
-
-      cartas.push(carta);
-      cartas.push(cartaDuplicada);
-      cartas.sort(() => 0.5 - Math.random());
-      cantidadPersonajes++;
-    } else {
-      continue;
-    }
+  while (id < 10) {
+    let carta = `<td><div id="a_${id}" onclick="voltear('a_',${id} )"><img src="${data[id].image}"></div></td>`;
+    let cartaDuplicada = `<td><div id="b_${id}" onclick="voltear('b_',${id} )"><img src="${data[id].image}"></div></td>`;
+    //console.log(id);
+    id++;
+    cartas.push(carta);
+    cartas.push(cartaDuplicada);
   }
+  // Mescla e impresion de cartas
+  console.log(cartas);
+  cartas.sort(() => Math.random() - 0.5);
   imprimirCartas(cartas);
 }
 
@@ -46,37 +47,35 @@ function imprimirCartas(cartas) {
     }
   }
 }
+
 // Logica del juego
 
-let cartasVolteadas = 0;
+let contadorVolteos = 0;
 let carta_1;
 let carta_2;
-let resultado_1;
-let resultado_2;
+let id_1;
+let id_2;
 
-function voltear(id) {
-  let element = document.getElementById(id);
-  cartasVolteadas++;
+function voltear(letra, num) {
+  contadorVolteos++;
 
-  if (cartasVolteadas == 1) {
-    resultado_1 = element;
-    carta_1 = id;
-  } else if (cartasVolteadas == 2) {
-    resultado_2 = element;
-    carta_2 = id;
-    cartasVolteadas = 0;
-    verificarGanador();
-    return id;
+  if (contadorVolteos == 1) {
+    carta_1 = letra + num;
+    id_1 = num;
+  } else if (contadorVolteos == 2) {
+    carta_2 = letra + num;
+    id_2 = num;
+    verificarCoincidencias();
+    contadorVolteos = 0;
   }
 }
 
-function verificarGanador() {
-  console.log(resultado_1);
-  console.log(resultado_2);
-
-  if (carta_1 == carta_2) {
-    console.log("Adivinaste");
-  } else {
-    console.log("No adivinaste");
+function verificarCoincidencias() {
+  if (id_1 == id_2) {
+    console.log("adivinaste");
+    console.log(carta_1);
+    console.log(carta_2);
+    document.getElementById(carta_1).innerHTML = "OK";
+    document.getElementById(carta_2).innerHTML = "OK";
   }
 }
